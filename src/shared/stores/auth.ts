7f9d5d75358ef3df7ee7813582fb50e0e054880c
eliscioter~ <attila433lehun@gmail.com>
2024-10-08 20:09:24 +0800

    feat: use redux persist and add store reducer

--- src/shared/stores/auth.ts
+++ src/shared/stores/auth.ts
@@ -1,15 +1,32 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { authAPI } from "../../pages/login/shared/api/auth";
 import { setupListeners } from "@reduxjs/toolkit/query";
+import branchReducer from "./branch";
+import { persistReducer, persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import { persist_config } from "../../configs/persistor";
+
+
+const reducer = combineReducers({
+  [authAPI.reducerPath]: authAPI.reducer,
+  branch: branchReducer,
+});
+
+const persistedReducer = persistReducer(persist_config, reducer)
 
 export const store = configureStore({
-  reducer: {
-    [authAPI.reducerPath]: authAPI.reducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authAPI.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(authAPI.middleware),
 });
 
 setupListeners(store.dispatch);
 
+export const persist_store = persistStore(store);
+
 export type IRootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
