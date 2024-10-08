fb4a880f9443227e56d2eda50afcc90ce1653a66
eliscioter~ <attila433lehun@gmail.com>
2024-10-08 20:06:53 +0800

    fix: simplify code and add semantics

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -7,24 +7,20 @@ import { Helmet } from "react-helmet-async";
 
 export default function Login() {
   const [is_modal_open, setIsModalOpen] = useState(false);
-  const [modal_title, setModalTitle] = useState("");
-  const [branch_id, setBranchId] = useState("");
 
   const modal_context = {
     modal_status: is_modal_open,
     setModalStatus: setIsModalOpen,
-    modal_title: modal_title,
-    setModalTitle: setModalTitle,
-    branch_id: branch_id,
-    setBranchId: setBranchId,
   };
   return (
     <>
       <LoginModalContext.Provider value={modal_context}>
         <Helmet title="Login" />
-        <Header />
+        <header>
+          <Header />
+        </header>
         <Branches />
-        {is_modal_open && <PasswordModal chosen_branch={modal_title} />}
+        {is_modal_open && <PasswordModal />}
       </LoginModalContext.Provider>
     </>
   );
