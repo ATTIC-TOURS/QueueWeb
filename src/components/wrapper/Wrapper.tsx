a53464c4845159ef70c853855f9af0ba570e07ee
eliscioter~ <attila433lehun@gmail.com>
2024-10-09 15:24:20 +0800

    fix: add condition

--- src/components/wrapper/Wrapper.tsx
+++ src/components/wrapper/Wrapper.tsx
@@ -1,17 +1,24 @@
+import { useSelector } from "react-redux";
+import { IRootState } from "../../shared/stores/app";
+
 export default function ModalWrapper({
-    children,
-    onClick,
-  }: {
-    children: React.ReactNode;
-    onClick?: () => void;
-  }) {
-    return (
-      <div
-        className="flex justify-center items-center fixed inset-0 bg-rose-100 bg-opacity-50 max-md:mx-3"
-        onClick={onClick}
-      >
-        {children}
-      </div>
-    );
-  }
-  
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  onClick?: () => void;
+}) {
+  const { active } = useSelector((state: IRootState) => state.modal);
+  return (
+    <>
+      {active && (
+        <div
+          className="flex justify-center items-center fixed inset-0 bg-rose-100 bg-opacity-50 max-xs:mx-3"
+          onClick={onClick}
+        >
+          {children}
+        </div>
+      )}
+    </>
+  );
+}
