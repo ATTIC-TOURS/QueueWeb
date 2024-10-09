d3a05da62495c32defd49c0edd940d2a807403e2
eliscioter~ <attila433lehun@gmail.com>
2024-10-09 15:22:00 +0800

    remove

--- src/pages/login/shared/context/modal-ctx.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export const LoginModalContext = createContext<{
-  modal_status: boolean;
-  setModalStatus: (status: boolean) => void;
-}>({
-  modal_status: false,
-  setModalStatus: () => {},
-});
