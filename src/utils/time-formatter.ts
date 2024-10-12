f61b2117f524281974725cee5cd2218a035b5053
eliscioter~ <attila433lehun@gmail.com>
2024-10-13 00:48:14 +0800

    fix: remove seconds

--- src/utils/time-formatter.ts
+++ src/utils/time-formatter.ts
@@ -2,8 +2,7 @@ export const FormatTime = (input: string) => {
   const raw_date = new Date(input);
 
   const hours = raw_date.getHours();
-  const minutes = raw_date.getMinutes();
-  const seconds = raw_date.getSeconds();
+  const minutes = raw_date.getMinutes().toString().padStart(2, "0");
 
   let formatted_hours = hours % 12;
   if (formatted_hours === 0) {
@@ -11,7 +10,7 @@ export const FormatTime = (input: string) => {
   }
   const period = hours < 12 ? "AM" : "PM";
 
-  const formatted_time = `${formatted_hours}:${minutes}:${seconds} ${period}`;
+  const formatted_time = `${formatted_hours}:${minutes} ${period}`;
 
   return formatted_time;
 };
