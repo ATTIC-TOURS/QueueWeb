41cd313d0ce6b66f0f09cb70a9d368209d46e52a
eliscioter~ <attila433lehun@gmail.com>
2024-10-13 00:51:44 +0800

    feat: add refetch

--- src/hooks/useCurrentStatus.ts
+++ src/hooks/useCurrentStatus.ts
@@ -5,7 +5,7 @@ import { useCurrentStatusQuery } from "../pages/dashboard/shared/api/queue";
 export function useCurrentStatus() {
   const id = useSelector((state: IRootState) => state.branch.id);
 
-  const { data: status, isSuccess: isCurrentStatusSuccess } = useCurrentStatusQuery(id ?? "");
+  const { data: status, isSuccess: isCurrentStatusSuccess, refetch } = useCurrentStatusQuery(id ?? "");
 
-  return { status, isCurrentStatusSuccess };
+  return { status, isCurrentStatusSuccess, refetch};
 }
