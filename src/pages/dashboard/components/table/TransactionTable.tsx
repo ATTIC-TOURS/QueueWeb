03ae77c676a14b1466aa351a5742a0cea2b48376
eliscioter~ <attila433lehun@gmail.com>
2024-10-13 00:54:13 +0800

    fix: add call highlight and initial implementation for filter

--- src/pages/dashboard/components/table/TransactionTable.tsx
+++ src/pages/dashboard/components/table/TransactionTable.tsx
@@ -1,23 +1,49 @@
-import { faFilter } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowRotateRight,
+  faFilter,
+  faSpinner,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useQueueQuery } from "../../shared/api/queue";
-import { useSelector } from "react-redux";
-import { IRootState } from "../../../../shared/stores/app";
 import { FormatTime } from "../../../../utils/time-formatter";
 import { useServices } from "../../../../hooks/useServices";
 import { useWindows } from "../../../../hooks/useWindows";
 import TableActions from "./components/actions/TableActions";
+import { useViewableStatus } from "../../../../hooks/useViewableStatus";
+import { useQueueTickets } from "../../../../hooks/useQueueTickets";
+import { useState } from "react";
+import { QueueTicketType } from "../../../../shared/types/queue-ticket";
 
 export default function TransactionTable() {
-  const id = useSelector((state: IRootState) => state.branch.id);
-
-  const { data: tickets, isSuccess: isTicketSuccess } = useQueueQuery(id ?? "");
+  const { isTicketSuccess, tickets, branch_id, refetch, isFetching } =
+    useQueueTickets();
 
   const { isServicesLoading, services_name } = useServices();
 
   const { isWindowLoading, windows_name } = useWindows();
 
-  if (!id) return <div>No branch ID available</div>;
+  const { viewableStatus } = useViewableStatus();
+
+  const [filtered_tickets, setFilteredTickets] = useState<
+    QueueTicketType[] | undefined
+  >([]);
+
+  const checkIfDone = (status_id: string) => {
+    const checked = viewableStatus.some(
+      (status) => status.id.toString() === status_id
+    );
+
+    return checked;
+  };
+
+  const handleServiceFilter = () => {
+    const filtered = tickets?.filter((ticket) =>
+      services_name(ticket.service_id)?.includes("Others")
+    );
+
+    setFilteredTickets(filtered);
+  };
+
+  if (!branch_id) return <div>No branch ID available</div>;
   if (!isTicketSuccess) return <div>Loading tickets...</div>;
   if (isServicesLoading) return <div>Loading services...</div>;
   if (isWindowLoading) return <div>Loading windows...</div>;
@@ -29,11 +55,17 @@ export default function TransactionTable() {
           <tr>
             <th className="py-4">
               Service
-              <FontAwesomeIcon icon={faFilter} size="1x" color="grey" />
+              <FontAwesomeIcon
+                icon={faFilter}
+                size="1x"
+                color="grey"
+                onClick={handleServiceFilter}
+              />
             </th>
             <th>
               Queue No
-              <FontAwesomeIcon icon={faFilter} size="1x" color="grey" />
+              {/* // Might have to remove filter for Queue no. */}
+              {/* <FontAwesomeIcon icon={faFilter} size="1x" color="grey" /> */}
             </th>
             <th>
               Visit Time
@@ -43,20 +75,71 @@ export default function TransactionTable() {
               Window
               <FontAwesomeIcon icon={faFilter} size="1x" color="grey" />
             </th>
+            <th className="float-end">
+              {isFetching ? (
+                <FontAwesomeIcon
+                  icon={faSpinner}
+                  size="1x"
+                  color="grey"
+                  className="float-end"
+                />
+              ) : (
+                <FontAwesomeIcon
+                  icon={faArrowRotateRight}
+                  size="1x"
+                  color="grey"
+                  onClick={refetch}
+                />
+              )}
+            </th>
           </tr>
         </thead>
         <tbody>
-          {tickets.map((item, index) => (
-            <tr key={index} className="border hover:bg-gray-700">
-              <td className="text-center">{services_name(item.service_id)}</td>
-              <td className="text-center">{item.queue_no}</td>
-              <td className="text-center">{FormatTime(item.created_at)}</td>
-              <td className="text-center">{windows_name(item.window_id)}</td>
-              <td className="py-3 text-center ">
-                <TableActions ticket={item} />
-              </td>
-            </tr>
-          ))}
+          {filtered_tickets && filtered_tickets.length > 0
+            ? filtered_tickets?.map((item, index) => (
+                <tr
+                  key={index}
+                  className={`border ${
+                    item.is_called && !checkIfDone(item.status_id.toString())
+                      ? "bg-slate-500"
+                      : ""
+                  }`}
+                >
+                  <td className="text-center">
+                    {services_name(item.service_id)}
+                  </td>
+                  <td className="text-center">{item.queue_no}</td>
+                  <td className="text-center">{FormatTime(item.created_at)}</td>
+                  <td className="text-center">
+                    {windows_name(item.window_id) ?? "unassigned"}
+                  </td>
+                  <td className="py-3 text-center ">
+                    <TableActions ticket={item} />
+                  </td>
+                </tr>
+              ))
+            : tickets?.map((item, index) => (
+                <tr
+                  key={index}
+                  className={`border ${
+                    item.is_called && !checkIfDone(item.status_id.toString())
+                      ? "bg-slate-500"
+                      : ""
+                  }`}
+                >
+                  <td className="text-center">
+                    {services_name(item.service_id)}
+                  </td>
+                  <td className="text-center">{item.queue_no}</td>
+                  <td className="text-center">{FormatTime(item.created_at)}</td>
+                  <td className="text-center">
+                    {windows_name(item.window_id) ?? "unassigned"}
+                  </td>
+                  <td className="py-3 text-center ">
+                    <TableActions ticket={item} />
+                  </td>
+                </tr>
+              ))}
         </tbody>
       </table>
     </div>
