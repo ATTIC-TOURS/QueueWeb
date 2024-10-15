035b1c1bbc6d554fb22d775a42ac732bb915b633
eliscioter~ <attila433lehun@gmail.com>
2024-10-15 23:03:33 +0800

    fix: remove logo

--- src/pages/dashboard/components/sidebar/Sidebar.tsx
+++ src/pages/dashboard/components/sidebar/Sidebar.tsx
@@ -1,15 +1,9 @@
 import { faClock } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import attic_logo from "../../../../assets/images/attic-logo-white.png";
 
 export default function Sidebar() {
   return (
     <div className="bg-red-800 pt-3 max-lg:hidden">
-      <img
-        src={attic_logo}
-        alt="Attic Tours Logo"
-        className="h-8 mx-auto"
-      />
       <div className="flex justify-center mt-5">
         <FontAwesomeIcon icon={faClock} color="white" className="h-10 w-1/2" />
       </div>
