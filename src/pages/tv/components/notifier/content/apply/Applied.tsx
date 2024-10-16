72010f46a97e4b5e8e9c7eedc69db383e6515c9f
eliscioter~ <attila433lehun@gmail.com>
2024-10-16 21:48:57 +0800

    chore: remove unused components

--- src/pages/tv/components/notifier/content/apply/Applied.tsx
+++
@@ -1,9 +0,0 @@
-export default function Applied() {
-  return (
-    <>
-      <h2 className="text-7xl">Thank you for applying</h2>
-      <h1 className="text-9xl font-bold py-6">JO2</h1>
-      <h3 className="text-6xl">Please wait to be called</h3>
-    </>
-  );
-}
--- src/pages/tv/components/notifier/content/call/Called.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Called() {
-  return (
-    <>
-        <h1 className="text-9xl pb-6">Window 4</h1>
-        <h3 className="text-9xl font-bold">JO2</h3>
-    </>
-  )
-}
