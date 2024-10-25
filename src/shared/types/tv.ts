e4bcc0798aa0258ea4d01538bd3dc01b5445077a
eliscioter~ <attila433lehun@gmail.com>
2024-10-26 00:40:28 +0800

    feat: add now serving type

--- src/shared/types/tv.ts
+++ src/shared/types/tv.ts
@@ -1,6 +1,15 @@
 import { z } from "zod";
-import { TVMarqueeSchema, WaitingCallSchema } from "../validators/tv";
+import {
+  NowServingListSchema,
+  NowServingSchema,
+  TVMarqueeSchema,
+  WaitingCallSchema,
+} from "../validators/tv";
 
 export type TVMarqueeType = z.infer<typeof TVMarqueeSchema>;
+
+export type NowServingType = z.infer<typeof NowServingSchema>;
+
+export type NowServingListType = z.infer<typeof NowServingListSchema>;
 
 export type WaitingCallType = z.infer<typeof WaitingCallSchema>;
--- src/shared/validators/tv.ts
+++ src/shared/validators/tv.ts
@@ -7,6 +7,37 @@ export const TVMarqueeSchema = z.array(
   })
 );
 
+export const NowServingListSchema = z.array(
+  z.object({
+    id: z.string(),
+    branch_id: z.string(),
+    service_id: z.string(),
+    window_id: z.string(),
+    queue_no: z.string(),
+    status_id: z.string(),
+    is_called: z.boolean(),
+    created_at: z.string(),
+    updated_at: z.string(),
+    code: z.string(),
+    name: z.string(),
+    email: z.string().email(),
+  })
+);
+export const NowServingSchema = z.object({
+  id: z.string(),
+  branch_id: z.string(),
+  service_id: z.string(),
+  window_id: z.string(),
+  queue_no: z.string(),
+  status_id: z.string(),
+  is_called: z.boolean(),
+  created_at: z.string(),
+  updated_at: z.string(),
+  code: z.string(),
+  name: z.string(),
+  email: z.string().email(),
+});
+
 export const WaitingCallSchema = z.object({
   name: z.string(),
   window_name: z.string(),
