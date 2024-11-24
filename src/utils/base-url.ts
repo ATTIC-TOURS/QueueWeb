7ff6b3c81f4287acc5ac7f908717ef8fd7af5bad
eliscioter~ <attila433lehun@gmail.com>
2024-11-24 17:55:20 +0800

    feat: for production

--- src/utils/base-url.ts
+++ src/utils/base-url.ts
@@ -4,10 +4,14 @@ type IProtocol = "http" | "https" | "ws" | "wss";
 export const getBaseUrl = (protocol: IProtocol) => {
   const hostname = window.location.hostname;
 
+  if (import.meta.env.VITE_MODE === "production") {
+    return `${protocol}://${import.meta.env.VITE_SERVER_BASE_URL}`;
+  }
+
   if (hostname === "localhost") {
     return `${protocol}://${import.meta.env.VITE_SERVER_LOCALHOST_BASE_URL}`;
   } else if (hostname.startsWith("192.168.")) {
-    return `${protocol}://${import.meta.env.VITE_SERVER_BASE_URL}`;
+    return `${protocol}://${import.meta.env.VITE_SERVER_MACHINE_BASE_URL}`;
   } else {
     return "";
   }
